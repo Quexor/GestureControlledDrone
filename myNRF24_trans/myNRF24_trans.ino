57dc477e8e2539933a7b82ee88bd5626121ebf77
mkan6397 <mkan6397@uni.sydney.edu.au>
2019-05-16 20:05:26 +1000

    printing out DMP data while sending over NRF - not tested yet

--- myNRF24_trans/myNRF24_trans.ino
+++ myNRF24_trans/myNRF24_trans.ino
@@ -3,12 +3,20 @@
 #include <printf.h>
 #include "CommUAV.h"
 
+//DMP
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
 
 // Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins. CE is 8, CS is 10.
 RF24 radio(8,10);
+MPU6050 mpu;
 
 byte add1[] = {0x34, 0xC3, 0x10, 0x10, 0x01};
-byte add2[] = {0x01, 0x10, 0x10, 0xC3, 0x34}; //Not working for some weird reason.
 
 uint8_t sendBuf[32];
 uint8_t sendCnt=0;
@@ -25,11 +33,32 @@ int Yaw = 1499;
 
 unsigned long counter = 0; //number that gets send
 
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
 void setup() {
   Serial.begin(115200);
   printf_begin();
   Serial.println("I'm alive");
-
+  DMPinit();
   NRF24init();
 }
 
@@ -46,6 +75,58 @@ void NRF24init() {
   radio.openWritingPipe(add1);
   radio.closeReadingPipe(1); //was opened by radio.begin();
   radio.printDetails();
+}
+
+void DMPinit() {
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+    
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+        Serial.println(F(")..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
 }
 
 void NRF24Send() {
@@ -146,6 +227,34 @@ void CommUAVUpload(uint8_t cmd)
     NRF24SendBuffer(sendBuf);
 }
 
+void processDMP() {
+  if (mpuInterrupt && fifoCount < packetSize) {
+        // try to get out of the infinite loop 
+        fifoCount = mpu.getFIFOCount();
+  }
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+  fifoCount = mpu.getFIFOCount();
+  // wait for correct available data length, should be a VERY short wait
+  while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+  // read a packet from FIFO
+  mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+  // track FIFO count here in case there is > 1 packet available
+  // (this lets us immediately read more without waiting for an interrupt)
+  fifoCount -= packetSize;
+
+  //display YAW PITCH ROLL
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+  mpu.dmpGetGravity(&gravity, &q);
+  mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+  Serial.print(ypr[0] * 180/M_PI);
+  Serial.print(",");
+  Serial.print(ypr[1] * 180/M_PI);
+  Serial.print(",");
+  Serial.println(ypr[2] * 180/M_PI);
+}
+
 void loop() {
   if ( Serial.available() )
   {
@@ -162,6 +271,5 @@ void loop() {
     if(c == 'T') {
       CommUAVUpload(MSP_SET_4CON);
     }
-  delay(10);
   }
 }
